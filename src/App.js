3be9e3f9ea8e372796fe512cd44fb0732c2e3318
Caio Barilli <caiobarilli@gmail.com>
2022-04-03 20:30:32 -0300

    Lint Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>QUICKDEV BLOG MVP</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import App from './App';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import './API/fakerApi';
+import React from 'react'
+import App from './App'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './API/fakerApi'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 
-const root = createRoot(container);
+const root = createRoot(container)
 
-root.render(<App tab="home" />);
+root.render(<App tab="home" />)
